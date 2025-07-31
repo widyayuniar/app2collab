e3222ef6752e598c5b4dcd678f960092f365f999
nopalss678 <nopalclass@gmail.com>
2025-07-31 13:04:03 +0700

    update terbaru

--- app/src/main/java/com/smktunas/app2collab/SplashActivity.kt
+++ app/src/main/java/com/smktunas/app2collab/SplashActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
-class splashactivity : AppCompatActivity() {
+class Splashactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this@splashactivity, MainActivity::class.java))
+            startActivity(Intent(this@Splashactivity, MainActivity::class.java))
             finish()
         }, 3000)
     }
