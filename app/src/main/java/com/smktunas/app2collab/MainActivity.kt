91e20f4453a04a71a79b828729553a76d83a8873
jewnurin <majdinadina4@gmail.com>
2025-07-31 09:46:49 +0700

    logic

--- app/src/main/java/com/smktunas/app2collab/MainActivity.kt
+++ app/src/main/java/com/smktunas/app2collab/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.smktunas.app2collab
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputName: EditText
+    private lateinit var inputKelas: EditText
+    private lateinit var btnSubmit: Button
+    private lateinit var txtResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        inputName = findViewById(R.id.nama)
+        inputKelas = findViewById(R.id.kelas)
+        btnSubmit = findViewById(R.id.tampilkan)
+        txtResult = findViewById(R.id.hasil)
+
+        btnSubmit.setOnClickListener {
+            val nama = inputName.text.toString().trim()
+            val kelas = inputKelas.text.toString().trim()
+            val hasil = "Nama: $nama\nKelas: $kelas"
+            txtResult.text = hasil
+
         }
     }
 }
