e1da77158cb5a3338b7aa944e59184ff342ed24b
nopalss678 <nopalclass@gmail.com>
2025-07-31 11:15:29 +0700

    add splash

---
+++ app/src/main/java/com/smktunas/app2collab/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.smktunas.app2collab
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000) // 3 detik
+    }
+}
